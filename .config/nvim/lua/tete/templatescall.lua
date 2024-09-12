bb09d6b8eadadfd018457420d99ce0636dbc9d64
Luis Fernando Umaña Castro <luis.umanacastro@ucr.ac.cr>
2024-09-11 19:52:36 -0600

    LaTeX funcionalitys update

--- .config/nvim/lua/tete/templatescall.lua
+++ .config/nvim/lua/tete/templatescall.lua
@@ -17,3 +17,8 @@ vim.api.nvim_create_user_command('InitText',
   '!xclip -sel clip < ~/.config/mytex/templates/C0S0-000.tex',
   {}
   )
+
+vim.api.nvim_create_user_command('InitBookExercise',
+  '!xclip -sel clip < ~/.config/mytex/templates/book-C00S00P000.tex',
+  {}
+  )
